58cd6304acb5c2b8ad9fcbf8f7da18ddcae63dd3
optimus <yudq3@mail2.sysu.edu.cn>
2019-11-28 15:21:22 +0800

    refactor 039

--- solutions/039 Combination Sum.py
+++ solutions/039 Combination Sum.py
@@ -7,23 +7,16 @@
 class Solution:
     def combination_sum(self, candidates: [int], target: int) -> [[int]]:
         candidates = sorted(candidates)
-        return self.combination_local_sum(candidates, target, {})
-
-    def combination_local_sum(self, candidates: [int], target: int, calculated: {}) -> [[int]]:
-        retrieved = set()
         result = []
         for i, candidate in enumerate(candidates):
             if candidate < target:
                 current_target = target - candidate
-                ano_candidates = self.combination_local_sum(candidates[i:], current_target, calculated)
-                if len(ano_candidates) > 0:
-                    calculated[current_target] = ano_candidates
+                ano_candidates = self.combination_sum(candidates[i:], current_target)
                 if len(ano_candidates) > 0:
                     for ano_can in ano_candidates:
                         temp = [candidate]
                         temp.extend(ano_can)
                         result.append(temp)
-                retrieved.add(candidate)
             elif candidate == target:
                 result.append([candidate])
         return result
